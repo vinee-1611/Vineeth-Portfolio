cfd05806ef7169879d11162540c58ba0e7a44e00
G Vineeth <vineethgattu065@gmail.com>
2025-12-10 04:51:56 +0000

    https://drive.google.com/file/d/1I2wbYpW9LpkFVjIDwnY6U241XmS2ZD6k/view?u

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -17,7 +17,7 @@ function ResumeButton() {
           title: "Opening Resume",
           description: "Your resume will open in a new tab.",
         });
-        window.open('https://drive.google.com/file/d/1J8C9maWbCK1bQTUe9csf8dkawuJHn0ax/view?usp=sharing', '_blank');
+        window.open('https://drive.google.com/file/d/1I2wbYpW9LpkFVjIDwnY6U241XmS2ZD6k/view?usp=drive_link', '_blank');
       }}
     >
       <Eye className="mr-2 h-5 w-5" />
